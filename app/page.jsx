1f6311fa6a78cb7b78f19bb9385459e0285b7de8
ivonck <greenhouse28@gmail.com>
2024-06-01 02:00:09 +0700

    botol

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import Wrapper from "@/components/layout/Wrapper";
 import MainHome from "../app/(homes)/home_1/page";
 
 export const metadata = {
-  title: "Home-1 || GoTrip - Travel & Tour React NextJS Template",
-  description: "GoTrip - Travel & Tour React NextJS Template",
+  title: "Sportaloka || Feel the Freedom with Every Step",
+  description: "Adventure portal",
 };
 
 export default function Home() {
