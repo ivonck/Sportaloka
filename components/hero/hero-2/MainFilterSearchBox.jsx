e04d0e17ec55b1221d77b702f900252dd6a93bdc
ivonck <greenhouse28@gmail.com>
2024-06-01 16:52:58 +0700

    botol masthead content

--- components/hero/hero-2/MainFilterSearchBox.jsx
+++ components/hero/hero-2/MainFilterSearchBox.jsx
@@ -10,7 +10,7 @@ const MainFilterSearchBox = () => {
   const Router = useRouter()
   return (
     <>
-      <div className="mainSearch -w-900 z-2 bg-white pr-10 py-10 lg:px-20 lg:pt-5 lg:pb-20 rounded-4 shadow-1 mt-40">
+      <div className="mainSearch -w-900 z-2 bg-white pr-10 py-10 lg:px-20 lg:pt-5 lg:pb-20 rounded-4 shadow-1 mt-10">
         <div className="button-grid items-center">
           <LocationSearch />
           {/* End Location */}
